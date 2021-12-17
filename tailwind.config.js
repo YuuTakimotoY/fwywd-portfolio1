2a6fa39a174938be086449d1206012e7721de3b9
YuuTakimotoY <yuu9701612038@gmail.com>
2021-12-17 14:07:07 +0900

    Tailwind.CSSの設定ファイルの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      'text-black': '#243c5a',
+      'text-green': '#008c8d',
+      'bg-green': '#6bc2c3',
+      'bg-black': '#262c3a',
+      'progress-pale': '#c5eaea',
+      'progress-deep-green': '#2bb9ba',
+      'progress-thin-green': '#c5eaea',
+      'border-green': '#c5eaea',
+    },
   },
   plugins: [],
-}
+};
