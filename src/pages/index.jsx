3e5eb11034ba9c6f01bef5a812505d86f7a45e52
YuuTakimotoY <yuu9701612038@gmail.com>
2021-12-17 23:54:36 +0900

    Skill部分を作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import About from '../components/About';
 import Hero from '../components/Hero';
 import Header from '../components/Shared/Header';
+import Skills from '../components/Skills';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
