f599bb5a7d71983694a084995e42822f849d67f1
YuuTakimotoY <yuu9701612038@gmail.com>
2021-12-17 22:22:44 +0900

    Title部分の共通コンポーネントを作成

---
+++ src/components/Shared/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title(props) {
+  return (
+    <div className={'flex gap-2 mx-0 mb-5 ' + props.position}>
+      <h3 className='text-2xl text-black'>{props.japaneseTitle}</h3>
+      <p className='my-auto text-sm text-green uppercase'>{props.englishTitle}</p>
+    </div>
+  );
+}
