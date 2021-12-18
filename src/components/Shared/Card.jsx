757140eb812116e738391431bbee591bb0953fb2
YuuTakimotoY <yuu9701612038@gmail.com>
2021-12-18 17:26:53 +0900

    Card共通部分で画像のサイズを変更できるように修正

--- src/components/Shared/Card.jsx
+++ src/components/Shared/Card.jsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Card(props) {
   return (
     <div className='px-8 w-full text-center'>
-      <Image src={props.img_src} alt={props.img_alt} width={240} height={180} objectFit='contain' />
+      <Image
+        src={props.img_src}
+        alt={props.img_alt}
+        width={props.img_width}
+        height={props.img_height}
+        objectFit='contain'
+      />
       <h3 className={'m-2 text-xl font-bold ' + props.titleColor}>{props.title}</h3>
       <p className='text-left'>{props.description}</p>
     </div>
--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -5,24 +5,27 @@ const values = [
   {
     img_src: '/value1.png',
     img_alt: 'value-1',
+    img_width: 200,
+    img_height: 200,
     title: '努力・成長',
-    titleColor: 'text-black',
     description:
       '何事でも時間をかけて取り組めば上位に入れると信じています。様々な失敗や成功経験により、努力次第で成長できると考えています。',
   },
   {
     img_src: '/value2.png',
     img_alt: 'value-2',
+    img_width: 200,
+    img_height: 200,
     title: '客観的な視点を重視',
-    titleColor: 'text-black',
     description:
       '主観的な視点で自分が満足していても、それが他者に評価されなければ意味がないと考えているので客観的な視点を持つことで、周囲が本当に求めていることを理解できるようになると考えています。',
   },
   {
     img_src: '/value3.png',
     img_alt: 'value-3',
+    img_width: 200,
+    img_height: 200,
     title: '知識',
-    titleColor: 'text-black',
     description:
       '新たらしい知識を得ることをとても楽しんでいます。書店などで自分の知らない知識を学ぶことが楽しく、いつもワクワクしながら活用しています。仕事面では、今後トレンドになりそうな技術を幅広く取り入れていきたいと考えています。',
   },
@@ -40,7 +43,10 @@ export default function Values() {
                 <Card
                   img_src={value.img_src}
                   img_alt={value.img_alt}
+                  img_width={value.img_width}
+                  img_height={value.img_height}
                   title={value.title}
+                  titleColor='text-black'
                   description={value.description}
                 />
               </>
