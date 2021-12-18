157ba088375b1cf4ae46868a0d6bc2edfe767f74
YuuTakimotoY <yuu9701612038@gmail.com>
2021-12-18 16:27:01 +0900

    Card部分コンポーネントのタイトルのテキストカラーをそれぞれで変えることができるようにする

--- src/components/Shared/Card.jsx
+++ src/components/Shared/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
   return (
     <div className='px-8 w-full text-center'>
       <Image src={props.img_src} alt={props.img_alt} width={240} height={180} objectFit='contain' />
-      <h3 className='m-2 text-xl font-bold text-green'>{props.title}</h3>
+      <h3 className={'m-2 text-xl font-bold ' + props.titleColor}>{props.title}</h3>
       <p className='text-left'>{props.description}</p>
     </div>
   );
