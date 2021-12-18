1da2fa10e036686b599d902f712dd51440dbd7d5
YuuTakimotoY <yuu9701612038@gmail.com>
2021-12-18 18:49:00 +0900

    Hero部分を微調整

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,13 @@ const subTitle = 'Programmer';
 
 export default function Hero() {
   return (
-    <div className='bg-header'>
-      <div className='flex flex-row items-center mx-auto'>
-        <div className='m-auto text-white'>
-          <h1 className='text-4xl font-bold'>{mainTitle}</h1>
-          <h2 className='text-xl'>{subTitle}</h2>
-        </div>
-        <div className='m-5'>
-          <Image src='/hero.png' alt='Hero' width={585} height={500} objectFit='contain' />
-        </div>
+    <div className='flex justify-between items-center py-10 bg-header'>
+      <div className='pl-24 text-white'>
+        <h1 className='text-[42px] font-bold'>{mainTitle}</h1>
+        <h2 className='pt-1 text-[28px]'>{subTitle}</h2>
+      </div>
+      <div className='pr-10'>
+        <Image src='/hero.png' alt='Hero' width={585} height={498} objectFit='contain' />
       </div>
     </div>
   );
